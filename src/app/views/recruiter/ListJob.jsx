d7d851916a1e85fe8020002f342f2b46dc9d2a62
Rohini Kawadkar <kawadkarrenu@gmail.com>
2024-02-12 22:16:13 +0530

    Project

--- src/app/views/recruiter/ListJob.jsx
+++ src/app/views/recruiter/ListJob.jsx
@@ -1,13 +1,16 @@
+import { Container, Card, Button, Box } from '@mui/material';
+import { Breadcrumb } from 'app/components';
+
 import React from 'react'
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux'
 import { getjobRequest } from 'slice/recruiter/createjobslice';
 
 export default function ListJob() {
 
-    const data = useSelector(y => y.jobs)
+    const { data, isloading, error, listdata } = useSelector((y) => y.jobs)
 
-    console.log(data);
+    console.log(listdata);
     const dis = useDispatch();
 
     useEffect(() => {
@@ -15,7 +18,44 @@ export default function ListJob() {
     }, [])
 
     return (
-        <div>ListJob</div>
+        <>
+            <Container>
+                <div>
+                    <Box className="breadcrumb">
+                        <Breadcrumb routeSegments={[{ name: 'ListJob', path: '/Recruiter' }, { name: 'ListJob' }]} />
+                    </Box>
+
+                    {isloading && <div>Loading</div>}
+                    {error && <div>{error}</div>}
+
+                    {data && (
+                        <ul className='col-sm-4'>
+                            {
+                                listdata?.map((v) => {
+                                    return (
+                                        <Card style={{ width: '18rem' }}>
+                                            <Card.Body>
+                                                <Card.Title>{v.title}</Card.Title>
+                                                <Card.Title>{v.skillsets}</Card.Title>
+                                                <Card.Title>{v.userId}</Card.Title>
+                                                <Card.Title>{v.jobType}</Card.Title>
+                                                <Card.Title>{v.salary}</Card.Title>
+                                                <Button variant='primary'>Go somewhere</Button>
+                                            </Card.Body>
+                                        </Card>
+                                    )
+
+
+                                })
+                            }
+                        </ul>
+
+
+
+                    )}
+                </div>
+            </Container>
+        </>
     )
 }
 
